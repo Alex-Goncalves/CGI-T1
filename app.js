d09631999f491070dce0c8ad2868f00aeb83c8fd
Alex-Goncalves <60947441+Alex-Goncalves@users.noreply.github.com>
2021-10-20 11:15:11 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -7,19 +7,22 @@ const table_width = 3.0;
 const MAX = 200;
 let table_height;
 let grid_spacing = 0.05;
+
 let vertices = [];
-let cargatingz = [];
+let protatingz = [];
+let eletratingz = [];
+
 var program;
+var program2;
+
 var uTheta = 0.0;
-var prot = 0;
-var elet = 0;
 var spaceUpOrDawn = true;
 
-function animate(time)
-{
+function animate(time) {
     window.requestAnimationFrame(animate);
     
     gl.clear(gl.COLOR_BUFFER_BIT);
+
     gl.useProgram(program);
 
     const uTable_dim_height = gl.getUniformLocation(program, "table_dim_height");
@@ -29,31 +32,68 @@ function animate(time)
     gl.uniform1f(utable_dim_width,  table_width/2.0);
 
     const colorloc = gl.getUniformLocation(program, "color");
-    const uThetaX = gl.getUniformLocation(program, "uTheta");
     const usize = gl.getUniformLocation(program, "size");
-    
-
-    gl.uniform1f(uThetaX, uTheta);
-    gl.uniform4fv(colorloc, [0.0, 1.0, 0.0, 1.0]);
-    gl.uniform1f(usize, 8.0);
-    if(spaceUpOrDawn)
-        gl.drawArrays(gl.POINTS, vertices.length, prot);
-
-    
-    gl.uniform1f(uThetaX, -uTheta);
-    gl.uniform4fv(colorloc, [1.0, 0.0, 0.0, 1.0]);
-    if(spaceUpOrDawn)
-        gl.drawArrays(gl.POINTS, vertices.length + prot, elet);
-
-    uTheta += 0.02;
-
-    console.log(cargatingz.length);
-
-    gl.uniform1f(uThetaX, 0);
 
     gl.uniform4fv(colorloc, [0.7, 0.0, 0.0, 0.3]);
     gl.uniform1f(usize, 4.0);
     gl.drawArrays(gl.POINTS, 0, vertices.length);
+
+
+    // program 2
+
+    gl.useProgram(program2);
+
+    drawCharges();
+
+    
+
+}
+
+function drawCharges() {    
+    
+
+    const colorloc = gl.getUniformLocation(program2, "color");
+    const usize = gl.getUniformLocation(program2, "size");
+    const uTable_dim_height = gl.getUniformLocation(program2, "table_dim_height");
+    gl.uniform1f(uTable_dim_height,  table_height/2.0);
+
+    const utable_dim_width = gl.getUniformLocation(program2, "table_dim_width");
+    gl.uniform1f(utable_dim_width,  table_width/2.0);
+    
+    gl.uniform1f(usize, 8.0);
+
+    var s = Math.sin( uTheta );
+    var c = Math.cos( uTheta );
+
+    uTheta = 0.02;
+
+    for(let i = 0; i < protatingz.length; i++) {
+        var x = (-s * protatingz[i][1] + c * protatingz[i][0]);
+        var y = (s * protatingz[i][0] + c * protatingz[i][1]);
+        protatingz.splice(i, 1, MV.vec2(x, y));
+    }
+
+    uTheta = -0.02;
+
+    for(let i = 0; i < eletratingz.length; i++) {
+        var x = (-s * eletratingz[i][1] + c * eletratingz[i][0]);
+        var y = (s * eletratingz[i][0] + c * eletratingz[i][1]);
+        eletratingz.splice(i, 1, MV.vec2(x, y));
+    }
+    
+
+    gl.bufferSubData(gl.ARRAY_BUFFER, vertices.length * MV.sizeof["vec2"], MV.flatten(protatingz));
+    gl.bufferSubData(gl.ARRAY_BUFFER, vertices.length * MV.sizeof["vec2"] + MAX * MV.sizeof["vec2"], MV.flatten(eletratingz));
+
+    const vPosition = gl.getAttribLocation(program2, "vPosition");
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+ 
+    gl.uniform4fv(colorloc, [0.0, 1.0, 0.0, 1.0]);
+    gl.drawArrays(gl.POINTS, vertices.length, protatingz.length);
+ 
+    gl.uniform4fv(colorloc, [1.0, 0.0, 0.0, 1.0]);
+    gl.drawArrays(gl.POINTS, vertices.length + MAX, eletratingz.length);
 }
 
 function setup(shaders)
@@ -62,6 +102,7 @@ function setup(shaders)
     gl = UTILS.setupWebGL(canvas);
 
     program = UTILS.buildProgramFromSources(gl, shaders["shader1.vert"], shaders["shader1.frag"]);
+    program2 = UTILS.buildProgramFromSources(gl, shaders["shader2.vert"], shaders["shader1.frag"]);
 
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -83,7 +124,7 @@ function setup(shaders)
     const aBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, aBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, vertices.length * MV.sizeof["vec2"] + 
-                                   MAX * MV.sizeof["vec2"], gl.STATIC_DRAW);
+                                   MAX * MV.sizeof["vec2"] + MAX * MV.sizeof["vec2"], gl.STATIC_DRAW);
 
     /**
      * Place vertice array in buffer
@@ -93,6 +134,7 @@ function setup(shaders)
     const vPosition = gl.getAttribLocation(program, "vPosition");
     gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
+
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     
@@ -138,28 +180,26 @@ function setup(shaders)
         const x = (((event.offsetX / canvas.width) * 2) - 1) * table_width/2.0;
         const y = (- (((event.offsetY / canvas.height) * 2) - 1)) * table_height/2.0;
         
-        /**
-        * Place cargatingz array in buffer
-        */
 
-        gl.bufferSubData(gl.ARRAY_BUFFER, vertices.length * MV.sizeof["vec2"] + (cargatingz.length % MAX) * 
-                        MV.sizeof["vec2"], MV.flatten(MV.vec2(x,y)));
+        
 
         if(upOrDawn) {
-            cargatingz.splice(0, 0, MV.vec2(x, y));
-            prot += 1;
-            console.log("neg");
-        } else {
-            cargatingz.push(MV.vec2(x, y));
-            elet += 1;
+            gl.bufferSubData(gl.ARRAY_BUFFER, vertices.length * MV.sizeof["vec2"] + 
+                    (protatingz.length % MAX) * MV.sizeof["vec2"], MV.flatten(MV.vec2(x,y)));
+            protatingz.push(MV.vec2(x, y));
             console.log("pos");
+        } else {
+            gl.bufferSubData(gl.ARRAY_BUFFER, vertices.length * MV.sizeof["vec2"] + 
+                    MAX * MV.sizeof["vec2"] + (eletratingz.length % MAX) * MV.sizeof["vec2"], MV.flatten(MV.vec2(x,y)));
+            eletratingz.push(MV.vec2(x, y));
+            console.log("neg");
         }
 
-        const vPosition = gl.getAttribLocation(program, "vPosition");
+        const vPosition = gl.getAttribLocation(program2, "vPosition");
         gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(vPosition);
 
     });
 }
 
-UTILS.loadShadersFromURLS(["shader1.vert", "shader1.frag"]).then(s => setup(s));
+UTILS.loadShadersFromURLS(["shader1.vert", "shader2.vert", "shader1.frag"]).then(s => setup(s));
--- shaders/shader1.vert
+++ shaders/shader1.vert
@@ -1,16 +1,13 @@
 uniform float table_dim_height;
 uniform float table_dim_width;
 attribute vec4 vPosition;
-uniform float uTheta;
 uniform float size;
 
 void main()
 {
-    float s = sin( uTheta );
-    float c = cos( uTheta );
     gl_PointSize = size;
-    gl_Position.x = -s * vPosition.y/table_dim_height + c * vPosition.x/table_dim_width;
-    gl_Position.y = s * vPosition.x/table_dim_width + c * vPosition.y/table_dim_height;
+    gl_Position.x = vPosition.x/table_dim_width;
+    gl_Position.y = vPosition.y/table_dim_height;
     gl_Position.z = 0.0;
     gl_Position.w = 1.0;
 }
